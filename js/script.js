0ced8d992cf0ae8fe8984350d8b25e7b3f72d5b7
dnov09 <dngige9@gmail.com>
2020-11-24 17:00:10 -0600

    finished js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 function init() {
     //add your javascrip between these two lines of code
     function alert_me() {
-
         var input = document.getElementById("entryinput").value;
         var username = document.querySelector("h2.center").innerHTML;
         alert(username + ": " + input);
-
+        results(input);
     }
+
+    function results(input) {
+        document.getElementById('textoutput').innerHTML = input;
+    }
+
     var alert_btn = document.getElementById('entrybutton');
     alert_btn.addEventListener('click', alert_me);
-
 }
 
 window.addEventListener('load', init);
